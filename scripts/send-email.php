39d11786694292ee3d3193705466fcb25f64b313
Allyssa Poulin <poulinallyssa@gmail.com>
2024-09-08 14:46:11 -0400

    update email submit

--- scripts/send-email.php
+++ scripts/send-email.php
@@ -1,12 +1,20 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = "Hey Allyssa, someone sent you a message from your personal portfolio. " + $name + ", " + $email + " says \n\n\n" + $_POST["message"];
 
-mail(
-    'allyssapoul@outlook.com',
-    $subject,
-    $message,
-    ):
+if(isset($_POST['submit'])
+    && !empty($_POST['name'])
+    && !empty($_POST['email'])
+    && !empty($_POST['message']) ) {
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = "Hey Allyssa, someone sent you a message from your personal portfolio. " . $name . ", " . $email . " says: \n\n\n" . $_POST['message'];
+    mail(
+         'allyssapoul@outlook.com',
+         'Someone sent a message!',
+         $message
+    );
+}
+
+header("Location: ../index.html");
+
 ?>
